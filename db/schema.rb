7aabe9fb46aafae205510adb35ab9bd98628edaf
richard jarram <rjarram@me.com>
2020-08-04 18:21:42 -0400

    2-0: built backend models and validates and associations; ran migrations and seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_024626) do
+ActiveRecord::Schema.define(version: 2020_08_04_221744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.bigint "restaurant_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_comments_on_restaurant_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_restaurants_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +46,7 @@ ActiveRecord::Schema.define(version: 2020_08_03_024626) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "restaurants"
+  add_foreign_key "comments", "users"
+  add_foreign_key "restaurants", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,20 @@ User.destroy_all
 
 ActiveRecord::Base.connection.reset_pk_sequence!(:restaurants)
 
-50.times do
-    user = User.create(email: Faker::Internet.email, password: 'qwerty')
-    10.times do
-      resto = Restaurant.create(
-      name: Faker::Restaurant.name,
-      address: Faker::Address.street_address,
-      user_id: user.id
-    )
-  
-    Comment.create(
-      content: Faker::Restaurant.review,
-      restaurant_id: resto.id,
-      user_id: user.id
-    )
-    end
+5.times do
+  user = User.create(email: Faker::Internet.email, password: 'qwerty')
+  10.times do
+    resto = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    user_id: user.id
+  )
+
+  Comment.create(
+    content: Faker::Restaurant.review,
+    restaurant_id: resto.id,
+    user_id: user.id
+  )
   end
-  puts 'seed ended'
+end
+puts 'seed ended'
