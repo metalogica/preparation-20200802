76c10afd4b55bab70f5ec35249bb71ba1c8b3867
richard jarram <rjarram@me.com>
2020-08-05 15:11:16 -0400

    3-1: built index route

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # /api/v1/restaurants
+  namespace :api, defaults: { format: :json } do
+    # api 
+    namespace :v1 do
+      # restaurants
+      resources :restaurants, only: [ :index ]
+    end
+  end
 end
