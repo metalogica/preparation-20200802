fdb45bf61d41516409c0e573be5fdd3194f61b0b
richard jarram <rjarram@me.com>
2020-08-05 16:23:35 -0400

    3-4: BUGFIX on Restaurants#index. @restauarants mispelled.

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
     def index
         #               Restaurant.all
-        @restauarants = policy_scope(Restaurant)
+        @restaurants = policy_scope(Restaurant)
     end
 end
